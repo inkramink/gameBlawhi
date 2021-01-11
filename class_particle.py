1f00a8bf2c3468acc46644f35bb7a8b0572554c2
inkramink <marinal2005.y.h@ya.ru>
2021-01-11 07:06:47 +0300

    убрана круговая платформа добавлены частицы при собирании кнопок

---
+++ class_particle.py
@@ -0,0 +1,50 @@
+import pygame
+import os
+import sys
+import random
+from class_blawhi import load_image, GRAVITY
+from main import size
+
+width, height = size
+screen_rect = (0, 0, width, height)
+all_sprites = pygame.sprite.Group()
+particles = pygame.sprite.Group()
+
+
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = [load_image("star.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(all_sprites)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = GRAVITY
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+
+    def create_particles(position):
+        # количество создаваемых частиц
+        particle_count = 20
+        # возможные скорости
+        numbers = range(-5, 6)
+        for _ in range(particle_count):
+            particles.add(Particle(position, random.choice(numbers), random.choice(numbers)))
